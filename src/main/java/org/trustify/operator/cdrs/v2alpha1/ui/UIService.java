1554d0eb1b5e57253e068d6cc90eebd221a86999
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2024-11-12 15:48:28 +0100

    merge

--- src/main/java/org/trustify/operator/cdrs/v2alpha1/ui/UIService.java
+++ src/main/java/org/trustify/operator/cdrs/v2alpha1/ui/UIService.java
@@ -7,7 +7,6 @@ import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDep
 import jakarta.enterprise.context.ApplicationScoped;
 import org.trustify.operator.Constants;
 import org.trustify.operator.cdrs.v2alpha1.Trustify;
-import org.trustify.operator.cdrs.v2alpha1.TrustifySpec;
 import org.trustify.operator.utils.CRDUtils;
 
 import java.util.Map;
